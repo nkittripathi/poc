78518edb41bdeec8ea3e5664663a5bc6f133f1c8
at186045 <at186045@192.168.1.101>
2017-09-25 19:10:58 +0530

    Second commit

--- thinkbig-continuous-integration/nifi-simple-processors/src/main/java/cicd/processors/simple/MyProcessor.java
+++ thinkbig-continuous-integration/nifi-simple-processors/src/main/java/cicd/processors/simple/MyProcessor.java
@@ -48,7 +48,7 @@ import java.util.Set;
 public class MyProcessor extends AbstractProcessor {
 
     public static final PropertyDescriptor MY_PROPERTY = new PropertyDescriptor
-            .Builder().name("MY_PROPERTY")
+            .Builder().name("NewProp")
             .displayName("My property")
             .description("Example Property")
             .required(true)
